8a9439dbd48cfbd883b376ecc0590f51c62a3b2a
Debjyoti <debjyotipaul385@gmail.com>
2014-02-09 05:22:45 +0530

    demo presentation ready

--- js/dartnews.js
+++ js/dartnews.js
@@ -4,22 +4,26 @@
 			cloudmade = L.tileLayer(cloudmadeUrl, {maxZoom: 19, attribution: cloudmadeAttribution});
 
 		var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-				attribution: ''
+				attribution: '',
+				maxZoom: 18
 			});
 		var Esri_WorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-				attribution: ''
+				attribution: '',
+				maxZoom: 18
 			});
 		var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-				attribution: ''
+				attribution: '',
+				maxZoom: 18
 			});
 
 		var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
-				attribution: ''
+				attribution: '',
+				maxZoom: 18
 			});
 
 
 		var map = new L.Map('map', {
-			layers: [cloudmade, Esri_WorldTopoMap,Esri_WorldStreetMap,Esri_WorldImagery],
+			layers: [cloudmade, Esri_WorldTopoMap,Esri_WorldStreetMap,Esri_WorldImagery,OpenStreetMap_BlackAndWhite],
 			center: new L.LatLng(28.64, 77.23),
 			zoom: 10,
 			fullscreenControl: true,
@@ -78,48 +82,157 @@
 			pinkPushPin = new MyIcon({iconUrl: 'images/marker_icon/pinkPushPin.png'}),
 			smallPinkFlag = new MyIcon({iconUrl: 'images/marker_icon/smallPinkFlag.png'}),
 			pinkIcon = new MyIcon({iconUrl: 'images/pink.png'});
-		var newsLayer = L.markerClusterGroup();
+
+		var newsLayer = L.markerClusterGroup(),
+			ReligiousNews = L.markerClusterGroup(),
+			WaterNews = L.markerClusterGroup(),
+			SportsNews = L.markerClusterGroup(),
+			WomenIssues = L.markerClusterGroup(),
+			Agriculture = L.markerClusterGroup(),
+			Education = L.markerClusterGroup(),
+			Industry = L.markerClusterGroup(),
+			WeatherNews = L.markerClusterGroup(),
+			PollutionEnvironment = L.markerClusterGroup(),
+			Events = L.markerClusterGroup(),
+			AccidentCalamity = L.markerClusterGroup(),
+			TransportNews = L.markerClusterGroup(),
+			Culture = L.markerClusterGroup(),
+			ElectricityLighting = L.markerClusterGroup(),
+			Crime = L.markerClusterGroup(),
+			Political = L.markerClusterGroup(),
+			Governance = L.markerClusterGroup(),
+			SocialIssues = L.markerClusterGroup(),
+			HealthHospital = L.markerClusterGroup(),
+			Development = L.markerClusterGroup(),
+			Others = L.markerClusterGroup();
+
 		var markers = [];		
-		for (var i = 0; i < addressPoints.length - 100; i++) {
+		for (var i = 0; i < 100 ; i++) {
 			var a = addressPoints[i];
-			var title = a[2];
-			var marker = new MyCustomMarker(new L.LatLng(a[0], a[1]), { icon: pinkFlagCurl, title: title });
-			marker.bindPopup( title +" " + a[0]+ " "+ a[1] + "<div id='cssmenu'> <ul>  <li class='has-sub'><a href='#'><span>Blogs</span></a> <ul  > <li><a href='http://www.labnol.org' target='_blank'><span>Digital Inspiration</span></a></li> <li><a href='http://www.extremetech.com' target='_blank'><span>Extreme Tech</span></a></li> <li><a href='http://www.lifehacker.com' target='_blank'><span>Lifehacker</span></a></li> <li><a href='http://blogs.ittoolbox.com' target='_blank'><span>IT Tool box</span></a></li> <li><a href='http://valleywag.com' target='_blank'><span>Valleywag</span></a></li> </ul> </li> <li class='has-sub'><a href='#'><span>Sports</span></a> <ul> <li><a href='http://espnstar.com' target='_blank'><span>ESPN Star</span></a></li> </ul> </li> </ul> </div>", {
-            	showOnMouseOver: true
-       	 	});
-       	 	markers.push(marker);
-			newsLayer.addLayer(marker);
+			var latLongList = a[0];
+			var title = eval('('+ a[1] +')');
+			// console.log(latLongList);
+			for( var j =0 ; j < latLongList.length; j++){
+				// console.log(latLongList[j][0] +"     "+ latLongList[j][1]);
+				var marker = new MyCustomMarker(new L.LatLng(latLongList[j][0], latLongList[j][1]), { icon: boardPin, title: title });
+				marker.bindPopup( "<div id='cssmenu' style='width: 200px;background: #555;margin-top:0.5cm;'> <ul>  <li class='has-sub'><a href='#'><span id='heading'>"+title.heading+"</span></a> <ul  > <li>LOCATION: " + latLongList[j][0]+ " "+ latLongList[j][1] +" <li>"+ title.contents+"</li>  </ul> </li><a href='"+title.link+"' target='_blank'><span id='category' style='display:none;'>"+title.category+"</span><span style='display:none; font-family:\"Playball\", cursive;font-size: 14px;color: #052A0A;'>   "+title.link+"</span></a>  </div>", {
+	            	showOnMouseOver: true
+	       	 	});
+	       	 	markers.push(marker);			
+       	 		switch(title.category){
+					case 'ReligiousNews': ReligiousNews.addLayer(marker); break;
+					case 'WaterNews': WaterNews.addLayer(marker); break;
+					case 'SportsNews': SportsNews.addLayer(marker); break;
+					case 'WomenIssues': WomenIssues.addLayer(marker); break;
+					case 'Agriculture': Agriculture.addLayer(marker); break;
+					case 'Education': Education.addLayer(marker); break;
+					case 'Industry': Industry.addLayer(marker); break;
+					case 'WeatherNews': WeatherNews.addLayer(marker); break;
+					case 'PollutionEnvironment': PollutionEnvironment.addLayer(marker); break;
+					case 'Events': Events.addLayer(marker); break;
+					case 'AccidentCalamity': AccidentCalamity.addLayer(marker); break;
+					case 'TransportNews': TransportNews.addLayer(marker); break;
+					case 'Culture': Culture.addLayer(marker); break;
+					case 'ElectricityLighting': ElectricityLighting.addLayer(marker); break;
+					case 'Crime': Crime.addLayer(marker); break;
+					case 'Political': Political.addLayer(marker); break;
+					case 'Governance': Governance.addLayer(marker); break;
+					case 'SocialIssues': SocialIssues.addLayer(marker); break;
+					case 'HealthHospital': HealthHospital.addLayer(marker); break;
+					case 'Development': Development.addLayer(marker); break;
+					default: marker.setIcon(blueInside); Others.addLayer(marker); break;
+       	 		}
+       	 	}
+			// newsLayer.addLayer(marker);
 		}
-		map.addLayer(newsLayer);
+		// map.addLayer(newsLayer);
+		map.addLayer(ReligiousNews);
+		map.addLayer(WaterNews);
+		map.addLayer(SportsNews);
+		map.addLayer(WomenIssues);
+		map.addLayer(Agriculture);
+		map.addLayer(Education);
+		map.addLayer(Industry);
+		map.addLayer(WeatherNews);
+		map.addLayer(PollutionEnvironment);
+		map.addLayer(Events);
+		map.addLayer(AccidentCalamity);
+		map.addLayer(TransportNews);
+		map.addLayer(Culture);
+		map.addLayer(ElectricityLighting);
+		map.addLayer(Crime);
+		map.addLayer(Political);
+		map.addLayer(Governance);
+		map.addLayer(SocialIssues);
+		map.addLayer(HealthHospital);
+		map.addLayer(Development);
+		map.addLayer(Others);
 
-		var newsLayer2 = L.markerClusterGroup();
-		for (var i = addressPoints.length - 100; i < addressPoints.length; i++) {
-			var a = addressPoints[i];
-			var title = a[2];
-			var marker = new MyCustomMarker(new L.LatLng(a[0], a[1]), { icon: greenFlagCheq, title: title });
-			marker.bindPopup( title +" " + a[0]+ " "+ a[1] + "<img src='http://api.webthumbnail.org?width=300&height=300&screen=1024&url=http://timesofindia.indiatimes.com/india/Cabinet-clears-Telangana-bill-Andhra-Pradesh-CM-ready-to-be-sacked/articleshow/30005769.cms' alt='Generated by WebThumbnail.org' />", {
-            	showOnMouseOver: true
-       	 	});
-       	 	markers.push(marker);
-			newsLayer2.addLayer(marker);
-		}
-		map.addLayer(newsLayer2);
+		map.removeLayer(Esri_WorldStreetMap);
+		map.removeLayer(OpenStreetMap_BlackAndWhite);
+		map.removeLayer(cloudmade);
 
+		map.removeLayer(ReligiousNews);
+		map.removeLayer(WaterNews);
+		map.removeLayer(SportsNews);
+		map.removeLayer(WomenIssues);
+		map.removeLayer(Agriculture);
+		map.removeLayer(Education);
+		map.removeLayer(Industry);
+		map.removeLayer(WeatherNews);
+		map.removeLayer(PollutionEnvironment);
+		map.removeLayer(Events);
+		map.removeLayer(AccidentCalamity);
+		map.removeLayer(TransportNews);
+		map.removeLayer(Culture);
+		map.removeLayer(ElectricityLighting);
+		// map.removeLayer(Crime);
+		map.removeLayer(Political);
+		map.removeLayer(Governance);
+		map.removeLayer(SocialIssues);
+		map.removeLayer(HealthHospital);
+		map.removeLayer(Development);
+		map.removeLayer(Others);
 
 
 		var baseMaps = {
-			"Imagery" : Esri_WorldImagery,
+			// "Imagery" : Esri_WorldImagery,
 		    "WorldStreet": Esri_WorldStreetMap,
 		    "TopoMap": Esri_WorldTopoMap,
-		    "BlackWhite" : OpenStreetMap_BlackAndWhite		    
+		    "BlackWhite" : OpenStreetMap_BlackAndWhite,
+		    "Cloudmade" : cloudmade
 		};
 
 		var overlayMaps = {
-		    "News": newsLayer,
-		    "News2": newsLayer2
+		    // "News": newsLayer,
+		    "ReligiousNews":ReligiousNews,
+			"WaterNews":WaterNews,
+			"SportsNews":SportsNews,
+			"WomenIssues":WomenIssues,
+			"Agriculture":Agriculture,
+			"Education":Education,
+			"Industry":Industry,
+			"WeatherNews":WeatherNews,
+			"PollutionEnvironment":PollutionEnvironment,
+			"Events":Events,
+			"AccidentCalamity":AccidentCalamity,
+			"TransportNews":TransportNews,
+			"Culture":Culture,
+			"ElectricityLighting":ElectricityLighting,
+			"Crime":Crime,
+			"Political":Political,
+			"Governance":Governance,
+			"SocialIssues":SocialIssues,
+			"HealthHospital":HealthHospital,
+			"Development":Development,
+			"Others":Others
 		};
 
-		L.control.layers(baseMaps, overlayMaps).addTo(map);
+		var control = L.control.activeLayers(baseMaps, overlayMaps);
+		control.addTo(map);
+
+
+
 
 		map.on('popupopen', function(e) {
   			var marker = e.popup._source;
@@ -128,18 +241,21 @@
 
   			// sidebar.hide();
   			setTimeout(function () {
-  				sidebar.setContent(marker._popup.getContent());
+  				var str = marker._popup.getContent();
+	            str = str.replace("style='width: 200px;background: #555;","style='width: 400px;background: #fff;");
+	            str = str.replace("display:none; ","display:block; ");
+  				sidebar.setContent(str);
     	        sidebar.show();
     	        // prepareMenu();
-	        }, 1000);
+	        }, 200);
   			
   			setTimeout(function(){
   				prepareMenu();
-  			},1500);
+  			},220);
 		});
 
         map.on('click', function () {
-        	rightSidebar.toggle();
+        	// rightSidebar.toggle();
             sidebar.hide();
         })
 
@@ -163,25 +279,61 @@
 			if(window.console) window.console.log('exitFullscreen');
 		});
 
+		Array.prototype.contains = function(obj) {
+		    var i = this.length;
+		    while (i--) {
+		        if (this[i] === obj) {
+		            return true;
+		        }
+		    }
+		    return false;
+		}
+
 		map.on('move', function() {
 	            // construct an empty list to fill with onscreen markers
 	            var inBounds = [],
 	            // get the map bounds - the top-left and bottom-right locations
-	                bounds = map.getBounds();
+	            bounds = map.getBounds();
 
 	            // for each marker, consider whether it is currently visible by comparing
 	            // with the current map bounds
-
+	            var prevHeading = '';
 	            for (var i = 0, len = markers.length; i < len; i++) {
 	                var marker = markers[i];
+
 	                if (bounds.contains(marker.getLatLng())) {
-	                    inBounds.push(marker._popup.getContent());
-	                    console.log(marker.options.title);
+	                	var str = marker._popup.getContent();
+	                	var $jQueryObject = $($.parseHTML(str));
+	                	var cat = $jQueryObject.find("#category").html();
+	                	var heading = $jQueryObject.find("#heading").html();
+	                	// $jQueryObject.find("#category").html();
+	                	if(prevHeading != heading){
+		                	var overlayLayers = control.getActiveOverlayLayers();
+		                	var activeLayerNames = [];
+		                	for ( var overlayId in overlayLayers){
+		                		// console.log(overlayLayers[overlayId].name +"    "+ cat);
+		                		if(overlayLayers[overlayId].name == cat){
+		                			// console.log(overlayLayers[overlayId].name +"    "+ cat);
+			                		str = str.replace("style='width: 200px;background: #555;","style='width: 400px;background: #fff;");
+				                	str = str.replace("display:none; ","display:block; ");
+				                    inBounds.push(str);	
+				                    break;
+		                		}
+		                	}
+		                	prevHeading = heading;
+	                	}
+	                	// if(activeLayerNames.contains($jQueryObject.find("#category").html())){
+	                	// 	str = str.replace("style='width: 200px;background: #555;","style='width: 400px;background: #fff;");
+		                // 	str = str.replace("display:none; ","display:block; ");
+		                //     inBounds.push(str);	
+	                	// }	                	
+	                    // console.log(marker.options.title);
 	                }
 	            }
 
-	            if(inBounds.length < 30){
+	            if(inBounds.length < 100){
 	            // display a list of markers.
+
 	            sidebar.setContent(inBounds.join('<br>\n'));
 	            prepareMenu();
 	        }
